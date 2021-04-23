2ff09b2a23a74a2e661da59f357f262e292747b8
codesbiome <github@codesbiome.com>
2021-04-23 07:24:43 +0530

    Commentary

--- tools/webpack/webpack.aliases.js
+++ tools/webpack/webpack.aliases.js
@@ -1,6 +1,12 @@
 const { createWebpackAliases } = require('./webpack.helpers');
 
-// Export aliases
+/**
+ * Export Webpack Aliases
+ *
+ * Tip: Some text editors will show the errors or invalid intellisense reports
+ * based on these webpack aliases, make sure to update `tsconfig.json` file also
+ * to match the `paths` we using in here for aliases in project.
+ */
 module.exports = createWebpackAliases({
   '@assets': 'assets',
   '@src': 'src',
