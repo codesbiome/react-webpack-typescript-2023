280effa986e568e4cac41c1d185ff3b069a7f7f2
codesbiome <github@codesbiome.com>
2022-10-26 09:12:05 +0530

    Webpack: Use webpack hmr and React refresh plugin

--- tools/webpack/webpack.plugins.js
+++ tools/webpack/webpack.plugins.js
@@ -1,9 +1,14 @@
+const webpack = require('webpack');
+const { inDev } = require('./webpack.helpers');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
+  inDev() && new webpack.HotModuleReplacementPlugin(),
+  inDev() && new ReactRefreshWebpackPlugin(),
   new HtmlWebpackPlugin({
     template: 'src/index.html',
     favicon: 'assets/images/logo.png',
@@ -13,4 +18,4 @@ module.exports = [
     filename: '[name].[chunkhash].css',
     chunkFilename: '[name].[chunkhash].chunk.css',
   }),
-];
+].filter(Boolean);
